ae019078191ddc909f67a75e62e613bab31738be
Zachary Sherman <165740629+zcsherm@users.noreply.github.com>
2025-10-24 14:08:45 -0700

    Create Body.py

---
+++ organism/Body.py
@@ -0,0 +1,11 @@
+"""
+Creates a simple organism for prototyping purposes
+"""
+
+from utilities import *
+import Chemicals
+
+class Body:
+
+    def __init__(self):
+        self._id = generate_id()
