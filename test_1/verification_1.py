3ebe802864ca61baef8b241484b1859b8b1d68cc
Zachary Sherman <165740629+zcsherm@users.noreply.github.com>
2025-10-27 11:37:36 -0700

    Update verification_1.py

--- test_1/verification_1.py
+++ test_1/verification_1.py
@@ -2,8 +2,8 @@ import unittest
 from Constructor import Decoder
 
 ORGAN_START = b'11111'
-O_PARAM_1 = b'00001'
-O_PARAM_2 = b'00101'
+O_PARAM_ONE = b'00001'
+O_PARAM_TWO = b'00101'
 GENE_START = b'11010'
 GENE_TYPE = b'1'          # Emitter
 GENE_FUNC = b'0110010'    # Exponential -> pow of 2
@@ -11,7 +11,8 @@ GENE_PARAMS = b'00010101' #Link to health and chem 5
 GENE_TWO = b'0'
 GENE_TWO_FUNC = b'11110101100001010' # Reverse sigmoid with coef = 1/86, mean= 10/128
 GENE_TWO_PARAMS = b'00100011' # Link to reaction rate and chem 3
-TEST_GENOME = ORGAN_START + O_PARAM_1 + O
+TEST_GENOME = ORGAN_START + O_PARAM_ONE + O_PARAM_TWO + GENE_START + GENE_TYPE + GENE_FUNC +GENE_PARAMS + GENE_TWO + GENE_TWO_FUNC + GENE_TWO_PARAMS
+
 
 
 class FirstTest(unittest.TestCase):
