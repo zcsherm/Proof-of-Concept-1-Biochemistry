6c554afab903606950c8ead881c45f9226afabdd
Zachary Sherman <165740629+zcsherm@users.noreply.github.com>
2025-10-31 12:36:32 -0700

    Update Organ.py

--- organism/Organ.py
+++ organism/Organ.py
@@ -15,6 +15,9 @@ class Organ:
     def set_dna_head(self, node):
         self._dna_head = node
 
+    def get_dna_hear(self):
+        return self._dna_head
+        
     def get_genome(self):
         if self._dna_head is not None:
             return self._dna_head.get_entire_genome()
